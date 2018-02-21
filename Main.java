112fefeb14358ae3ac5d28f8703c9cf2dd87cdb1
Unknown <wesley.bekkers@gmail.com>
2018-02-21 20:40:38 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.yacht.java;
+
+import com.yacht.java.model.Kalamata;
+import com.yacht.java.model.Ligurian;
+import com.yacht.java.model.Olive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Olive> olives = new ArrayList<>();
+        olives.add(new Kalamata());
+        olives.add(new Kalamata());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+
+        OlivePress press = new OlivePress();
+        int totalOil = press.getOil(olives);
+
+        System.out.println("You got " + totalOil + " units of oil");
+
+    }
+
+}
---
+++ model/Ligurian.java
@@ -0,0 +1,15 @@
+package com.yacht.java.model;
+
+public class Ligurian extends Olive {
+
+    public Ligurian() {
+
+        super(OliveName.LIGURIAN, OliveColor.BLACK, 5);
+
+    }
+
+    @Override
+    public String getOrigin() {
+        return "Italy";
+    }
+}
